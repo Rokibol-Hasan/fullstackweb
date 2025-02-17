490abb7560fe38224f28bab64b1014b32bd452cc
Rokibol-Hasan <rokibol.hasancse@gmail.com>
2025-02-17 15:41:33 +0600

    garbage deleted

--- bubblesort.py
+++
@@ -1,8 +0,0 @@
-myarr= [94,5,34,66,232,6564,32323,46,3]
-arraylen = len(myarr)
-print(arraylen)
-for i in range(arraylen-1):
-    for j in range(arraylen-i-1):
-        if(myarr[j]>myarr[j+1]):
-            myarr[j],myarr[j+1] = myarr[j+1],myarr[j]
-print('sorted array:',myarr)
